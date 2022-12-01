8e14ed078f7635486d740b7063ffb9ca653654f2
Rehema Owino <sayeakinyi@gmail.com>
2022-12-01 15:47:15 -0800

    Add int arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    
+    arg = len(argv) - 1
+    
+    if arg == 0:
+        print('{:d}'.format(arg))
+    else:
+        result = []
+
+        for i in range(1, arg + 1):
+            result.append(int(argv[i]))
+        print('{}'.format(sum(result)))
+
