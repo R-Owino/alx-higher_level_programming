eaa15b1ab8feeafd54519b9dec1865c9bd480909
Rehema Owino <sayeakinyi@gmail.com>
2022-12-01 15:48:57 -0800

    Add int arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     from sys import argv
-    
+
     arg = len(argv) - 1
-    
+
     if arg == 0:
         print('{:d}'.format(arg))
     else:
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         for i in range(1, arg + 1):
             result.append(int(argv[i]))
         print('{}'.format(sum(result)))
-
