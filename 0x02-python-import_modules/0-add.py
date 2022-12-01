929a2ae24e495d6e4174686594c93ac569a7c8a1
Rehema Owino <sayeakinyi@gmail.com>
2022-12-01 14:52:08 -0800

    Add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,9 @@
 
 if __name__ == "__main__":
 
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    sum = add_0.add(a, b)
 
-    print('{:d} + {:d} = {:d}'.format(a, b, sum))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
