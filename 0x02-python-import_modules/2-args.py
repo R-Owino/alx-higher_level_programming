7e576e02c037c22b34c5353fd135aacdfc1be7ed
Rehema Owino <sayeakinyi@gmail.com>
2022-12-01 15:31:01 -0800

    Argument count & display

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
 
     index = 1
 
-    if argv_count is 0:
+    if argv_count == 0:
         print("{:d} arguments.".format(argv_count))
-    elif argv_count is 1:
+    elif argv_count == 1:
         print("{:d} argument:".format(argv_count))
         print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
