bc4aa440bf93d1e862275a7430c5444aba26b5cb
Rehema Owino <sayeakinyi@gmail.com>
2022-12-05 16:17:54 -0800

    No cC

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    str_new = ''
+    for character in my_string:
+        if character not in 'Cc':
+            str_new += character
+    return str_new
+
+
+if __name__ == "__main__":
+    no_c()
