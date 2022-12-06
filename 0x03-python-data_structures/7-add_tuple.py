a5b88fadfeed62cd1c6dd0ffe4939bc35ed28026
Rehema Owino <sayeakinyi@gmail.com>
2022-12-05 18:38:50 -0800

    Add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+
+if __name__ == "__main__":
+    add_tuple()
