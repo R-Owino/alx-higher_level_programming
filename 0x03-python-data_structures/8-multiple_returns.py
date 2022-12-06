d0899599c02cd75df8599922628c9d23888b2daf
Rehema Owino <sayeakinyi@gmail.com>
2022-12-05 18:51:00 -0800

    Length and char 0

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    count = len(sentence)
+    if count > 0:
+        first_char = sentence[0]
+    else:
+        'None'
+
+    sentence_tuple = count, first_char
+    return sentence_tuple
+
+if __name__ == "__main__":
+    multiple_returns()
