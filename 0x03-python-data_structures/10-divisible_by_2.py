19842f5565d9be8e28958fab0e476814884e1d1f
Rehema Owino <sayeakinyi@gmail.com>
2022-12-06 11:28:16 -0800

    2 divisibles

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for count, i in enumerate(my_list):
+        if i % 2 == 0:
+            new_list[count] = True
+        else:
+            new_list[count] = False
+    return new_list
+
+
+if __name__ == "__main__":
+    divisible_by_2()
