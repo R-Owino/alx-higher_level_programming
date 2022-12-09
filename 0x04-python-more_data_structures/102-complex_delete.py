f99cc4c310a7f09d568374d4097d5801779bfa24
Rehema Owino <sayeakinyi@gmail.com>
2022-12-09 01:28:44 -0800

    Another delete dict key

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    target = []
+    for key, val in a_dictionary.items():
+        if val is value:
+            target.append(key)
+
+    for  i in target:
+        del a_dictionary[i]
+    return a_dictionary
