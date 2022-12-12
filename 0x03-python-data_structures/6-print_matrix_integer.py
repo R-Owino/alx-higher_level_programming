a84b9b58fdc6f92ac96dad427a4825d1c766e719
Rehema Owino <sayeakinyi@gmail.com>
2022-12-12 12:08:37 -0800

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,3 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         print(' '.join('{:d}'.format(col) for col in row))
-
-
-if __name__ == "__main__":
-    print_matrix_integer()
