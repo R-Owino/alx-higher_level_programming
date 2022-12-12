54c49932d58be02d0fabc0c7f87a62b86bbf7d1c
Rehema Owino <sayeakinyi@gmail.com>
2022-12-12 12:06:24 -0800

    Replace 2

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    
     my_new_list = my_list[:]
     if 0 <= idx < len(my_list):
         my_new_list[idx] = element
         return my_new_list
     return my_list
-
-
-if __name__ == "__main__":
-    new_in_list()
