611a4e1efe5ee76766cb120d8c65f506d3a93622
Rehema Owino <sayeakinyi@gmail.com>
2023-01-10 23:19:13 -0800

    Reads a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''Defines a function read_file'''
+
 
 def read_file(filename=""):
+    '''Reads a file and prints it to stdout'''
+
     with open(filename, mode='r', encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end='')
