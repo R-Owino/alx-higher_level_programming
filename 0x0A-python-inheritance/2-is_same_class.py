9df6357ade5ee3a4ed3649f8179b81f9f89dc03c
Rehema Owino <sayeakinyi@gmail.com>
2023-01-11 09:09:30 -0800

    Instance of specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Defines a function is_same_class'''
+
+
+def is_same_class(obj, a_class):
+    '''Returns True if the object is exactly an instance of the specified class
+    otherwise False
+
+    Args:
+        obj: instance of a class
+        class: the class
+    '''
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
