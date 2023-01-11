6907ee68b22f62ae49524267ed2919e5132139af
Rehema Owino <sayeakinyi@gmail.com>
2023-01-11 01:43:37 -0800

    Class Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Defines a class Student'''
+
+
+class Student:
+    '''Defines a student'''
+    
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a student
+        Args:
+            first_name(str): first name
+            last_name(str): last name
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''Retrieves a dict rep'n of a student instance'''
+        return self.__dict__
