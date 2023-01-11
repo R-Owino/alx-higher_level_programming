4865c0a0abc16e21f969a3ab8ead9320f34f4bed
Rehema Owino <sayeakinyi@gmail.com>
2023-01-10 23:47:54 -0800

    Write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
 '''Defines a function write_file'''
 
+
 def write_file(filename="", text=""):
     '''Writes a string to a text file
+    Args:
+        filename: file to write to
+        text: text to write
 
     Returns: Number of characters written
     '''
 
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
-
-'''
-    with open(filename, encoding='utf-8') as f:
-        while True:
-            line = f.readline()
-
-            if not line:
-                break
-
-            wordlist = line.split()
-
-            # counting the characters
-            c = 0
-            for word in wordlist:
-                for c in word:
-                    c += 1
-                print(c)
-'''
