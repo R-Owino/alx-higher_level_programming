aa5b71010f57b199979fa80d70a906d2fde9b356
Rehema Owino <sayeakinyi@gmail.com>
2023-01-16 13:35:55 -0800

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,18 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Writes JSON string rep'n of list_objs to a file
+
+        Args:
+            list_objs(list): list of instances inheriting from Base
+        '''
+        filename = cls.__name _ + '.json'
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write('[]')
+            else:
+                list_dict = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(list_dict))
