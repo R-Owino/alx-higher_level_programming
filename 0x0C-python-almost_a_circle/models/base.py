0924b14adaac47407cf167353ebe69d3533617e7
Rehema Owino <sayeakinyi@gmail.com>
2023-01-16 14:06:03 -0800

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -75,3 +75,14 @@ class Base:
                 new = cls(1)
             new.update(**dictionary)
             return new
+
+    @classmethod
+    def load_from_file(cls):
+        '''Returns a list of instances'''
+        filename = cls.__name__ + '.json'
+        try:
+            with open(filename, 'r') as f:
+                list_dict = Base.from_json_string(f.read())
+                return [cls.create(**d) for d in list_dict]
+        except IOError:
+            return []
