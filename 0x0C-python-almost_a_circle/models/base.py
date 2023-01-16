1fc700adf5a02d954626a9a91e10d25063db3bc5
Rehema Owino <sayeakinyi@gmail.com>
2023-01-16 13:44:54 -0800

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,14 @@ class Base:
             else:
                 list_dict = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Returns the list of the JSON string rep'n of json_string
+
+        Args:
+            json_string(list): string rep'ing list of dictionaries
+        '''
+        if json_string is None or json_string == []:
+            return '[]'
+        return json.loads(json_string)
