062cad8ef40abbac0bf4f10c0ebc50ae691ebad9
Rehema Owino <sayeakinyi@gmail.com>
2023-02-16 10:06:09 -0800

    Order in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of second_table
+-- results should display both the score and the name (in this order)
+-- records should be ordered by score in descending order
+SELECT score, name FROM second_table
+ORDER BY score DESC;
