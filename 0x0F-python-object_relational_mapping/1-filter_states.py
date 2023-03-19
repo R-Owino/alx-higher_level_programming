24a8b135abbcd34841bdcb4b06b48748fb9241f4
Rehema Owino <sayeakinyi@gmail.com>
2023-03-19 05:16:14 -0700

    States that start with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,25 +5,27 @@ import MySQLdb
 from sys import argv
 
 """
-script that lists all states from the database hbtn_0e_0_usa
+script that lists all states starting wit N from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
     # Getting username, password and database name from commandline args
-    # username = argv[1]
-    # password = argv[2]
-    # database = argv[3]
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
     # connect to MySQL server running on lovalhost at port 3306
     db = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1],
-            password=argv[2], database=argv[3])
+            host="localhost", port=3306, user=username,
+            password=password, database=database)
 
     # cursor object
     cursor = db.cursor()
 
     # SQL query to fetch all states from states table
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("""SELECT * FROM states
+            WHERE name LIKE BINARY 'N%'
+            ORDER BY id ASC""")
 
     # fetch all rows
     rows = cursor.fetchall()
