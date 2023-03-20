bc5a0e129ee15848cd353395238aa0259c7c9120
Rehema Owino <sayeakinyi@gmail.com>
2023-03-19 23:40:54 -0700

    Cities in states

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-""" prints the State object with the name passed as argument from the database
 """
-import sys
+prints the State object with the name passed as argument from the database
+"""
+
+from sys import argv
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import (create_engine)
@@ -10,11 +12,28 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Getting username, password and database name from commandline args
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # create enginne that connects to the database
+    engine = create_engine(f'''mysql+mysqldb://{username}:{password}@localhost:
+            3306/{database}''')
+
+    # Create tables in the database if they don't exist
     Base.metadata.create_all(engine)
+
+    # create a configured session class
     Session = sessionmaker(bind=engine)
+
+    # create a Session instance
     session = Session()
+
+    # query database to get all city objects
     for instance in (session.query(State.name, City.id, City.name)
                      .filter(State.id == City.state_id)):
         print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
+
+    # close session
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """
 Contains the class definition of a City
 """
+
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
