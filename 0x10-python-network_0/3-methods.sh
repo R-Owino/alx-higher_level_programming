a6e9e80c8fd4f931eed036d91f0dfe9aed33b1cf
Rehema Owino <sayeakinyi@gmail.com>
2023-03-30 18:42:12 -0700

    curl HTTP methods allowed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-curl -sILX OPTIONS "$1" | grep -i allow
+curl -sIL "$1" | grep "Allow" | cut -d " " -f2-
