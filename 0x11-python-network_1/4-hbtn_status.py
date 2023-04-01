1ad82e3e538e163f9a5e2c5cc0321c09add8fee3
R-Owino <sayeakinyi@gmail.com>
2023-03-31 18:56:23 -0700

    URL status using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+using requests package
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content = response.text.strip()
+
+    print('Body response:')
+    print(f'\t- type: {type(content)}')
+    print(f'\t- content: {content}')
