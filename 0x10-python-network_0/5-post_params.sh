6a6ff353f4096f673e9b39e8c5ec770b4b67f8bd
R-Owino <sayeakinyi@gmail.com>
2023-04-02 08:25:39 -0700

    curl POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  script that takes in a URL as an argument, sends a POST request to the URL
-curl -sSL -d "email=test@gmail.com&subject=I'll always be here for PLD" "$1"
+# script that takes in a URL as an argument, sends a POST request to the URL, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I'll always be here for PLD" "$1"
