0375a76a0f317536c0f15a8472520b7ab8b01024
R-Owino <sayeakinyi@gmail.com>
2023-05-04 02:22:06 +0000

    Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const $ = window.$;
-$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
-  $.each(data.results, function(i, movie) {
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  $.each(data.results, function (i, movie) {
     $('ul#list_movies').append('<li>' + movie.title + '</li>');
   });
 });
